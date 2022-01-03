12f2961244c9ea746485c31c6a964c898177367c
Tahier Hussain <tahierhussain2205@gmail.com>
2022-01-03 12:06:29 +0530

    Added Routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,19 @@
+import { Route, Switch } from 'react-router-dom';
+import AdminLoginPage from './pages/AdminLogin';
+import HomePage from './pages/Home';
+
 function App() {
   return (
-    <div>Hello World!</div>
+    <div>
+      <Switch>
+        <Route path='/' exact>
+          <HomePage />
+        </Route>
+        <Route path='/login' exact>
+          <AdminLoginPage />
+        </Route>
+      </Switch>
+    </div>
   );
 }
 
---
+++ client/src/pages/AdminLogin.js
@@ -0,0 +1,9 @@
+function AdminLoginPage() {
+  return (
+    <div>
+      <h1>Admin Login Page!</h1>
+    </div>
+  );
+}
+
+export default AdminLoginPage;
